f3590e10c680dcaf65b07749b24d0f1744300781
Reza Geshani <rezageshaniweb@gmail.com>
2025-04-09 02:42:17 +0330

    Update WorkItems.js

--- src/data/WorkItems.js
+++ src/data/WorkItems.js
@@ -96,6 +96,20 @@ const workItems = [
                 name: 'Coming Soon',
                 description: 'So many project are in coming soon'
             },
+            {
+                id: 2,
+                icon: 'next',
+                link: 'https://bama-next-router.vercel.app/',
+                name: 'Bama-Next-Router',
+                description: 'This project is for router configuration in Next.'
+            },
+            {
+                id: 3,
+                icon: 'next',
+                link: 'https://food-xi-sand-38.vercel.app/',
+                name: 'Food',
+                description: 'This project is for routing and getting information from the API in Next.'
+            },
         ]
     },
     {
